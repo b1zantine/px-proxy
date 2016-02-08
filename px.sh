a98f0177eddeb9055d42b6db70f8b77e4b5e8605
Sudar Abisheck <sudarabisheck@gmail.com>
2016-02-08 17:43:24 +0530

    restart required message added

--- px.sh
+++ px.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+
+#The MIT License (MIT)
+#
+#Copyright (c) 2016 Sudar Abisheck
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+
 _DOMAIN="proxy.ssn.net"
 _PORT=8080
 _IGNORE_PROXY="'*.ssn.net'"
@@ -16,6 +40,7 @@ _ENV_CONF_DATA="http_proxy=\"http://$_DOMAIN:$_PORT/\"\nhttps_proxy=\"https://$_
 ######  APT proxy settings
 unset_apt_proxy () {
 	awk '!/^Acquire::\w+::proxy/' $_APT_FILE_PATH > temp && chmod 0644 temp && sudo mv -f temp $_APT_FILE_PATH
+	sudo chgrp root $_APT_FILE_PATH
 }
 
 set_apt_proxy () {
@@ -25,7 +50,8 @@ set_apt_proxy () {
 
 ######  Environment proxy settings
 unset_env_proxy () {
-	awk '!/^\w+_proxy/' $_ENV_FILE_PATH > temp && chmod 0644 temp && sudo mv -f temp $_ENV_FILE_PATH 
+	awk '!/^\w+_proxy/' $_ENV_FILE_PATH > temp && chmod 0644 temp && sudo mv -f temp $_ENV_FILE_PATH
+	sudo chgrp root $_ENV_FILE_PATH
 }
 
 set_env_proxy () {
@@ -73,6 +99,8 @@ set_proxy () {
 	set_apt_proxy
 	set_env_proxy
 	echo "\t** System proxy  -  OK"
+	echo "\n***Note***\nEnvironment proxy settings takes effect only after a restart."
+	echo "To remove Environment variables temporarily use *** unset <env variable> *** command.\n"
 }
 
 unset_proxy () {
@@ -80,6 +108,8 @@ unset_proxy () {
 	unset_apt_proxy
 	unset_env_proxy
 	echo "\t** System proxy  -  REMOVED"
+	echo "\n***Note***\nEnvironment proxy settings takes effect only after a restart."
+	echo "To remove Environment variables temporarily use *** unset <env variable> *** command.\n"
 }
 
 proxy_stat () {
